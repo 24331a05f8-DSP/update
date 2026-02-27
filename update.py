42650c93c4814802ad708f9964c391d0c27faa25
24331a05f8-DSP <24331a05f8@mvgrce.edu.in>
2026-02-27 06:59:01 +0530

    Add files via upload

---
+++ update.py
@@ -0,0 +1,10 @@
+employee = {
+    "id" : 56,
+    "name" : "DSP"
+}
+
+employee["salary"] = 50000
+print("After adding salary :", employee)
+
+employee["salary"] = 60000
+print("After updating salary :", employee)
